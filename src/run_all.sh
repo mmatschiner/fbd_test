519c877f9221e974a5c381a5b2d12960c7c2b50f
Michael Matschiner <michaelmatschiner@mac.com>
2018-07-10 23:27:23 +0200

    Updated script

--- src/run_all.sh
+++ src/run_all.sh
@@ -6,8 +6,9 @@ bash simulate_datasets.sh
 # Prepare input for BEAST2.
 bash prepare_beast_input.sh
 
-# Run all BEAST2 analyses.
+# Run all beast analyses.
 bash run_beast.sh
+exit # Continue with the commands below once beast analyses have finished. If necessary, resume mcmc chains (see content of run_beast.sh for details).
 
 # Get run statistics.
 bash get_run_statistics.sh
